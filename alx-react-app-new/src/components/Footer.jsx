a839750546d5b28d064b0b2a8fd9f237c754aee2
oladimeji <oladimejisaeed@gmail.com>
2025-07-19 09:03:56 +0100

    added inline styling to the header, main content and the footer

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{padding : "10px 20px"}}>
+      <p style={{fontSize : "12px", color : "blue" }}>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{backgroundColor : "red", textAlign : "center", padding : "10px 20px"}}>
+      <h1 style={{color : "white"}}>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+export default Header;
