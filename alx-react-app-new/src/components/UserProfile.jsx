04c604abd62927d161f76352c3d7dbe1a4038c58
oladimeji <oladimejisaeed@gmail.com>
2025-07-19 12:17:16 +0100

    made corrections on userprofile component

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile(props){
     return (
         <div style={{border : "1px solid grey", margin : "10px", padding : "10px", borderRadius : "5px", boxShadow: "0 2px 4px black", background : "lightBlue"}}>
             <h2 style={{ fontSize : "32px", color : "blue", fontWeight : "bold"}}>{props.name}</h2>
-            <p style={{fontSize : "16px", color : "red"}}>Age: {props.age}</p>
+            <p>Age: <span  style={{fontSize : "16px", color : "red"}}>{props.age}</span></p>
             <p style={{fontSize : "16px", color : "red"}}>Bio: {props.bio}</p>
         </div>
     );
