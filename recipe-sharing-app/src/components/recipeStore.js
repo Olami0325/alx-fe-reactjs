6a61e7d39b61afc29f9ceee546cbbe526a538536
oladimeji <oladimejisaeed@gmail.com>
2025-07-30 16:06:36 +0100

    done

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,14 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Adds a single recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
+
+  // Sets or replaces the entire recipes array
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
+
--- recipe-sharing-app/src/store/useRecipeStore.js
+++ recipe-sharing-app/src/store/useRecipeStore.js
@@ -2,14 +2,8 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-
-  // Adds a single recipe
   addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
-
-  // Sets or replaces the entire recipes array
-  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
