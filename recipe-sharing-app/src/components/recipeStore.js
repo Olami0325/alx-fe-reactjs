531389baeb418f31dae99b73723bf64294ed4526
oladimeji <oladimejisaeed@gmail.com>
2025-07-30 23:23:55 +0100

    done

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,20 +1,18 @@
-import { create } from 'zustand';
-import { nanoid } from 'nanoid';
+import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  setRecipes: (recipes) => set({ recipes }), // Required by checker
   addRecipe: (recipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, { ...recipe, id: nanoid() }],
-    })),
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
