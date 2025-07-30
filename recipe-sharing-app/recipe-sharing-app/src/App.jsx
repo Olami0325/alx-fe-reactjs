336cb12346bbeb89b9d0df96e2906c78ec3a4b7c
oladimeji <oladimejisaeed@gmail.com>
2025-07-30 15:02:18 +0100

    Initial commit: Recipe Sharing App with Zustand

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import "./App.css";
+
+function App() {
+ 
+  return (
+    <>
+      <div style={{ maxWidth: "600px", margin: "0 auto", padding: "2rem" }}>
+        <h1>🍽️ Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+    </>
+  );
+}
+
+export default App;
