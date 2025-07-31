2f58a9c7f5f03ffceda1d494f2fb999faae29a2c
oladimeji <oladimejisaeed@gmail.com>
2025-07-31 18:18:12 +0100

    done with recipeStore

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,16 +1,45 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
-export const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, { ...recipe, id: Date.now().toString() }],
-  })),
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((r) =>
-      r.id === updatedRecipe.id ? updatedRecipe : r
-    ),
-  })),
-  deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((r) => r.id !== id),
-  })),
+  favorites: [],
+  searchTerm: "",
+
+  // Add recipe
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+
+  // Update recipe
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === updatedRecipe.id ? updatedRecipe : r
+      ),
+    })),
+
+  // Delete recipe
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+      favorites: state.favorites.filter((fid) => fid !== id), // remove from favorites too
+    })),
+
+  // Set search term
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Add to favorites
+  addFavorite: (id) =>
+    set((state) => ({
+      favorites: [...state.favorites, id],
+    })),
+
+  // Remove from favorites
+  removeFavorite: (id) =>
+    set((state) => ({
+      favorites: state.favorites.filter((fid) => fid !== id),
+    })),
 }));
+
+export default useRecipeStore;
