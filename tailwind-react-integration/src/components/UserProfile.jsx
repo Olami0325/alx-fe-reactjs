0b94111c976756b5ceaf2f45bfdedea8a4546b34
oladimeji <oladimejisaeed@gmail.com>
2025-08-08 22:02:23 +0100

    added resposive userProfile component with advanced transition and interactivity

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,20 @@
+import profilePic from "../assets/Profile_Pics.jpg";
+
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img
+        src={profilePic}
+        alt="User"
+        className="rounded-full sm:w-24 sm:h-24 md:h-36 md:w-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
+      <p className="text-grey-600 sm:text-sm md:text-base">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
