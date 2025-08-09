7cf46294fd7d3ccdc4c02d2e05ac7dd3cf8366e5
oladimeji <oladimejisaeed@gmail.com>
2025-08-09 10:20:24 +0100

    done

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ function UserProfile() {
         className="rounded-full sm:w-24 sm:h-24 md:h-36 md:w-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
-      <p className="text-grey-600 sm:text-sm md:text-base">
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
